0d61ab9cc49adfcf299d2b63e0577e9dbce50d37
thatsully <mosullivan22@wustl.edu>
2024-02-07 15:12:51 -0600

    added a player character with basic controls

---
+++ Assets/Code/SkiControls.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code
+{
+    public class SkiControls : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //move left
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-0.1f, 0, 0);
+            }
+
+            //move right
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(0.1f, 0, 0);
+            }
+            
+            //slow down
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += new Vector3(0, 0.1f, 0);
+            }
+            
+            //speed up
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position += new Vector3(0, -0.1f, 0);
+            }
+            
+        }
+    }
+}
